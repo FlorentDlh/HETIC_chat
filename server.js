05b7a19e6fe63a90715d2a4e717437e23898bd40
florentdlh <florentdela60@gmail.com>
2019-02-20 09:58:53 +0100

    maj

--- server.js
+++ server.js
@@ -1,14 +1,54 @@
-/*
+/* 
 Imports
 */
-// 
+    // NodeJS
+    require('dotenv').config();
+    const express = require('express');
+    const path = require('path');
+    const bodyParser = require('body-parser');
+    const ejs = require('ejs');
 
-/*
+    // Inner
+    const mainRouter = require('./routes/main.router');
+//
 
+
+/* 
+Configuration
 */
-// 
+    const server = express();
+    const port = process.env.PORT;
 
-/*
+    class ServerClass{
 
+        init(){
+            // Config du dossier client
+            server.set( 'views', __dirname + '/www' );
+            server.use( express.static(path.join(__dirname, 'www')) );
+
+            // Config du moteur de rendu
+            server.set( 'view engine', 'ejs' );
+
+            // Configurer les routes
+            server.use('/', mainRouter);
+
+
+            // Lancer le serveur
+            this.launch();
+        }
+
+        launch(){
+            server.listen(port, () => {
+                console.log(`Server is active on port ${port}`);
+            });
+        }
+
+    }
+//
+
+ 
+/* 
+Démarrer le serveur
 */
-// 
+    new ServerClass().init();
+//
